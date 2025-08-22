423cfdc43fa151a335ab84ac3c8575956f122b15
Rashad-7 <rashadsonbol987@gmail.com>
2025-08-22 14:46:03 +0300

    Update error.response.js

--- src/utils/res/error.response.js
+++ src/utils/res/error.response.js
@@ -8,8 +8,8 @@ return (req,res,next)=>{
  }
  export const globalErrorHandling=(error,req,res,next) =>{
     if (process.env.MOOD==="DEV") {
-        return res.status(error.cause||400).json({massage:error.massage,error,stack:error.stack})
+        return res.status(error.cause||400).json({message:error.message,error,stack:error.stack})
     }
-    return res.status(error.cause||400).json({massage:error.massage,error})
+    return res.status(error.cause||400).json({massage:error.message,error})
 
  }
